f461e466dd590ae0454d4fdb48e9a926ed46b755
Vikas Bishnoi <vikaskbm@gmail.com>
2021-11-06 09:54:52 +0530

    Refactor styles

--- varta/webviews/components/Sidebar.svelte
+++ varta/webviews/components/Sidebar.svelte
@@ -1,25 +1,34 @@
 <script lang="ts">
     let loading = false;
-    let user = false;
-    let page = '';
+    let user = true;
+    let page = "chat";
 </script>
 
 {#if loading } 
     <div>loading...</div>
 {:else if user}
-    {#if page==='chat'}
-        <!-- <Todos user={user} {accessToken}/> -->
+    {#if page==='list'}
+        <!-- <Search Component> -->
+        <!-- <List Friends Component/> -->
         <button>
             go to contact
         </button>
-    {:else}
+    {:else if page==='chat'}
+        <!-- <Chat Component></Chat> -->
+        <div>CHat one person here</div>
+    {:else} 
         <div>Contact me here: asdsasagsd</div>
-        <button>
-            go back
+        <button class="bottom-btn">
+            Back
         </button>
     {/if}
-    <button>logout</button>
+
+    <button style="position:relative;
+        margin-top:450%;">Logout</button>
+
 {:else}
-    <button>Login</button>
-    <button>Signup</button>
+    <button style="position:relative;
+        margin-top: 5rem">Login</button>
+    <button style="position:relative;
+        margin-bottom: 1rem">Signup</button>
 {/if}
