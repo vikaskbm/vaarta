d6f7d0d85d084ad6da5a11aa3887ab4b2f57286a
Vikas Bishnoi <vikaskbm@gmail.com>
2021-11-06 06:58:44 +0530

    Add sidebar component

---
+++ varta/webviews/components/Sidebar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    let loading = false;
+    let user = false;
+    let page = '';
+</script>
+
+{#if loading } 
+    <div>loading...</div>
+{:else if user}
+    {#if page==='chat'}
+        <!-- <Todos user={user} {accessToken}/> -->
+        <button>
+            go to contact
+        </button>
+    {:else}
+        <div>Contact me here: asdsasagsd</div>
+        <button>
+            go back
+        </button>
+    {/if}
+    <button>logout</button>
+{:else}
+    <button>Login</button>
+    <button>Signup</button>
+{/if}
