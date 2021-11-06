92a9d7e1f2c46c75e9f13f7a63ffd39c671e7281
Vikas Bishnoi <vikaskbm@gmail.com>
2021-11-06 06:19:44 +0530

    Setup and Style sidebar

--- varta/src/extension.ts
+++ varta/src/extension.ts
@@ -13,13 +13,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('varta.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from varta!');
-	});
+	
+	context.subscriptions.push(
+		vscode.commands.registerCommand('varta.helloWorld', () => {
+			// The code you place here will be executed every time your command is executed
+			// Display a message box to the user
+			vscode.window.showInformationMessage('Hello World from varta!');
+		})
+	);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vstodo.refresh', async () => {
+			// HelloWorldPanel.kill();
+			// HelloWorldPanel.createOrShow(context.extensionUri);
+			await vscode.commands.executeCommand("workbench.action.closeSidebar")
+			await vscode.commands.executeCommand("workbench.view.extension.vstodo-sidebar-view")
+			// setTimeout(() => {
+			// 	vscode.commands.executeCommand("workbench.action.webview.openDeveloperTools");
+			// }, 500);
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
