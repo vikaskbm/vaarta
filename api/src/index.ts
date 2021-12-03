8ac4dc66af340830063f33bfa94b33d7fbc21c45
Vikas Bishnoi <vikaskbm@gmail.com>
2021-12-03 21:07:46 +0530

    Connect database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
 
+require("dotenv").config();
 
 const main = async () => {
     const app = express();
+  
+    mongoose.connect(
+        process.env.MONGO_URL,
+        () => {
+          console.log("Connected to MoczxcxzngoDB");
+        }
+    );
+
     app.listen(8800, ()=> {
         console.log("Backend running");
     })
