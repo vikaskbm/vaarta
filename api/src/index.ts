9cd4c809c496b01b91c7f53002213b389739faf9
Vikas Bishnoi <vikaskbm@gmail.com>
2021-12-04 03:06:35 +0530

    Setup user and environment variables

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
+import helmet from "helmet";
 
+const userRoute = require('./router/users');
 require("dotenv").config();
 
 const main = async () => {
@@ -13,7 +15,15 @@ const main = async () => {
           console.log("Connected to MoczxcxzngoDB");
         }
     );
+    
+    // Middleware
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan("common"));
 
+    app.get("/", (req, res)=> {
+        res.send("Hello there")
+    })
     app.listen(8800, ()=> {
         console.log("Backend running");
     })
