05f57375a6e86328dc655d45690e5b76246e3196
Vikas Bishnoi <vikaskbm@gmail.com>
2021-12-23 16:46:46 +0530

    Resetup backend js

--- api/src/router/auth.ts
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-
-
-export default router.get("/", (req:any, res:any)=> {
-    res.send("auth route")
-})
--- api/src/router/users.ts
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-
-
-export default router.get("/", (req:any, res:any)=> {
-    res.send("user route")
-})
