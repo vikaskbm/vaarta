62e8ae250a400b44ad207550603a574ae1d47269
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-01 23:42:44 +0530

    add yarn dependency

--- api/index.js
+++ api/index.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const passport = require("passport");
+const GitHubStrategy = require('passport-github').Strategy;
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 
 dotenv.config();
 
+// database connect
 mongoose.connect(
   process.env.MONGO_URL,
   () => {
@@ -17,16 +20,41 @@ mongoose.connect(
   }
 );
 
+// passport serialize
+passport.serializeUser(function(user, done) {
+  done(null, user.accessToken);
+});
 
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+app.use(passport.initialize());
 
+// passport strategy
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:8100/auth/github/callback"
+  },
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
+  function(_, __, profile, cb) {
+    console.log(profile)
+    cb(null, {accessToken: '', refreshToken: ''})
+  }
+));
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github'),
+  function(req, res) {
+    res.send("You logged in correctly")
+  }); 
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
 
 app.listen(8100, () => {
   console.log("Backend server is running!");
--- api/models/User.js
+++ api/models/User.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
     {
+        id: Number,
         username: String,
-        email: String,
-        password: String
+        name: String,
     }
 );
 
