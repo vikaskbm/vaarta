565651de5038c0a0388f5da7530f1fc94f1fc902
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-02 02:15:53 +0530

    Create authentication functionality in frontendcd

--- varta/src/authenticate.ts
+++ varta/src/authenticate.ts
@@ -1,3 +1,27 @@
+import * as vscode from 'vscode';
+import * as polka from "polka";
+import { apiBaseUrl } from './constants';
+
 export const authenticate = () => {
-    console.log("called")
+    const app = polka()
+    
+    app.get('/auth/:token', async (req, res) => {
+        const { token } = req.params;
+        if(!token) {
+            res.end(`<h1>Something went wrong</h1>`)
+        }
+        console.log(token)
+
+        res.end(`<h1>Auth successful. Close this window</h1>`)
+    })
+    app.listen(54321, (err: Error) => {
+        if(err) {
+            vscode.window.showErrorMessage(err.message)
+        } else {
+            vscode.commands.executeCommand(
+                'vscode.open', 
+                vscode.Uri.parse(`${apiBaseUrl}/auth/github`)
+            )
+        }
+    })
 }
---
+++ varta/src/constants.ts
@@ -0,0 +1,1 @@
+export const apiBaseUrl = 'http://127.0.0.1:8100'
